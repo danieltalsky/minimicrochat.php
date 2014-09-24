63eb055630c3ba4513fb3219a33eadaf04938541
Daniel Talsky <danieltalsky@gmail.com>
2014-09-24 18:19:35 -0400

    First commit of application and instructions

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-minimicrochat.php
-=================
+# minimicrochat.php
 
-This simplest possible chat solution that will run on most shared hosting.
+minimicrochat.php is the smallest, most compact, chat client I could code and test in under two hours.  It uses only vanilla PHP5 with embedded SQLite
+
+## Installation Instructions
+
+1. Verify that your hosting environment has sqlite enabled for PHP.  Most do. 
+2. Upload index.php to a folder in the webroot. (ie. public_html/yo/)
+3. Verify that your script / apache will have access to write the sqlite file to the directory.
+4. Provide a link to the directory (http://yourdomain/yo/) to anyone you want to chat with and begin chatting.
+
+## The story
+
+A friend of mine had all chat avenues blocked by his company.  We really like chatting during the day, and so I took two hours and banged out the simplest possible php script that would make it so we could easily chat.  My design goals were:
+
+- No AJAX
+- No database server
+- Lightning fast performance
+- As configuration free as possible
+- As few moving pieces as possible
+- Should work on almost any shared hosting (especially mine)
+- Valid HTML5
+- Should be able to distinguish users from each other.
+
+### Note: Security Concerns
+
+This software is only for ad-hoc chatting when other avenues of chat are restricted.  It should be free from SQL injection risks but if a spammer discovers it they could easily grow your SQLite file to huge proportions.
+
+It also stores the 2nd, 3rd, and 4th portions of the reported IP address as an RGB color, so if having this part of your IP address recorded with each record is a concern then this probably isn't the software for you without modification.
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * minimicrochat.php - the simplest chat server I could write in two hours
+ */
+$db=createdb();
+if ($_POST['yo']){sendmessage($db,$_POST['yo']);}
+$res=getmessages($db, 25);
+$msgs=$res->fetchAll();
+?><!doctype html>
+<html lang="en">
+<head><meta charset="utf-8"><title>yo</title>
+<body onload="document.yo.yo.focus();" style="font-size: 12px; font-family: helvetica, sans-serif;">
+<ul>
+<?php if (count($msgs)) { foreach ($msgs as $msg) { ?>
+<li>
+    <span style="color: rgb(<?php print($msg['rgb']); ?>);">Yo: </span>
+    <?php print(htmlspecialchars($msg['message'])) ?>
+</li>
+<?php }} ?> 
+</ul>
+<form method="post" name="yo">
+<input style="width: 80%;" type="text" autocomplete="off" name="yo" />
+<button type="submit">yo</button>
+</form></body></html>
+<?php
+// lib
+function createdb() {
+    $dayhash = md5(date('Ymd'));
+    $db = new PDO('sqlite:yo.sqlite3');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->exec("CREATE TABLE IF NOT EXISTS messages_$dayhash (
+                    id INTEGER PRIMARY KEY,
+                    rgb TEXT, 
+                    message TEXT)");
+    return $db;
+}
+function sendmessage($db, $msg) {
+    $dayhash = md5(date('Ymd'));
+    $insert = "INSERT INTO messages_$dayhash (rgb, message) 
+                VALUES (:rgb, :message)";
+    $stmt = $db->prepare($insert);
+    $stmt->bindParam(':message', $msg);
+    $rgb = rgbfromip();
+    $stmt->bindParam(':rgb', $rgb);
+    $stmt->execute();
+}
+function getmessages($db, $num=25) {
+    $dayhash = md5(date('Ymd'));
+    $res = $db->query("SELECT * FROM messages_$dayhash 
+                           ORDER BY id DESC LIMIT $num");
+    return $res;
+}
+function rgbfromip() {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $ipa = explode(".", $ip);
+    return (int)$ipa[1] . ',' . (int)$ipa[2] . ',' . (int)$ipa[3]; 
+} 
