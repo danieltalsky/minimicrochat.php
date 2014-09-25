57c8ef4a65febcd94efd91b71c60437cd0db08a9
Daniel Talsky <danieltalsky@gmail.com>
2014-09-25 17:53:46 -0400

    Fixed mistaken query

--- index.php
+++ index.php
@@ -44,13 +44,13 @@ function createdb() {
 
 function sendmessage($msg) {
 	$dayhash = md5(date('Ymd'));
-	db()->query("INSERT INTO messages_$dayhash (rgb, message) 
+	query("INSERT INTO messages_$dayhash (rgb, message) 
 				VALUES (?, ?)", array(rgbfromip(), $msg));
 }
 
 function getmessages($num=25) {
 	$dayhash = md5(date('Ymd'));
-	return db()->query("SELECT * FROM messages_$dayhash ORDER BY id DESC LIMIT :num", array($num));
+	return query("SELECT * FROM messages_$dayhash ORDER BY id DESC LIMIT :num", array($num));
 }
 
 function rgbfromip() {
